9d85b101ac718ecec53e44039b2b1f7e5a1d5bba
zimahsan <sitihazimahhassan@gmail.com>
2018-10-24 17:03:20 +0800

    chat_rooms

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+
+  def name
+    email.split('@')[0]
+  end
+end
